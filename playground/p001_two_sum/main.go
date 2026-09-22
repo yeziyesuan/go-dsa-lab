1a7962b06651addef17ad642365c590bd4e9e27a
yeziyesuan <2494227233@qq.com>
2026-09-22 21:17:42 +0800

    playground: p001 两数之和（暴力 O(n^2)，三组示例上机通过）

---
+++ playground/p001_two_sum/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 两数之和
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("示例1:", twoSum([]int{2, 7, 11, 15}, 9), "期望 [0 1]")
+	fmt.Println("示例2:", twoSum([]int{3, 2, 4}, 6), "期望 [1 2]")
+	fmt.Println("示例3:", twoSum([]int{3, 3}, 6), "期望 [0 1]")
+}
