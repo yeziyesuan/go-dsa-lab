141d04b8cb354934fb7f368f27bc1c12f4530b7e
yeziyesuan <2494227233@qq.com>
2026-09-24 16:16:27 +0800

    playground: p001 两数之和改哈希表 O(n)（04_map 练习：make + comma-ok 查存在）

--- playground/p001_two_sum/main.go
+++ playground/p001_two_sum/main.go
@@ -3,13 +3,26 @@ package main
 import "fmt"
 
 // 两数之和
-func twoSum(nums []int, target int) []int {
+/*func twoSum(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] == target {
 				return []int{i, j}
 			}
 		}
+	}
+	return nil
+}*/
+//哈希
+func twoSum(nums []int, target int) []int {
+	hashtable := make (map[int]int)
+	for i := 0; i < len(nums); i++ {
+		v := nums[i]
+		if _, ok :=hashtable[target-v];ok{
+			return []int{hashtable[target-v],i}
+		}else{
+			hashtable[v] = i}
+		
 	}
 	return nil
 }
